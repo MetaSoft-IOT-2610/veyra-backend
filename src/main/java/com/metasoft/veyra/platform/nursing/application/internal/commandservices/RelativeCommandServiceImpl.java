9f687cfa6a375eb5059fc147ca2a39f928a07ce1
RenatoCY <renato.calvo2@hotmail.com>
2026-05-29 01:18:36 -0500

    feat(nursing): enhance relative management with email validation and user assignment

--- src/main/java/com/metasoft/veyra/platform/nursing/application/internal/commandservices/RelativeCommandServiceImpl.java
+++ src/main/java/com/metasoft/veyra/platform/nursing/application/internal/commandservices/RelativeCommandServiceImpl.java
@@ -1,13 +1,12 @@
 package com.metasoft.veyra.platform.nursing.application.internal.commandservices;
-
 import com.metasoft.veyra.platform.nursing.application.internal.outboundservices.acl.ExternalIamService;
 import com.metasoft.veyra.platform.nursing.domain.model.aggregates.Relative;
+import com.metasoft.veyra.platform.nursing.domain.model.commands.AssignUserToRelativeCommand;
 import com.metasoft.veyra.platform.nursing.domain.model.commands.CreateRelativeCommand;
 import com.metasoft.veyra.platform.nursing.domain.services.RelativeCommandService;
 import com.metasoft.veyra.platform.nursing.infrastructure.persistence.jpa.repositories.RelativeRepository;
+import com.metasoft.veyra.platform.shared.domain.model.valueobjects.EmailAddress;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 @Service
 public class RelativeCommandServiceImpl implements RelativeCommandService {
     private final RelativeRepository relativeRepository;
@@ -18,16 +17,23 @@ public class RelativeCommandServiceImpl implements RelativeCommandService {
     }
     @Override
     public Long handle(CreateRelativeCommand command) {
-        var userId = externalIamService.createUser(
-                command.username(),
-                command.password(),
-                List.of("ROLE_FAMILIAR")
-        );
-        if (relativeRepository.existsByUserId(userId)) {
-            throw new IllegalArgumentException("Family already exists for this user");
-        }
-        var familiar = new Relative(userId);
-        relativeRepository.save(familiar);
-        return familiar.getId();
+    if (relativeRepository.findByEmailAddress(new EmailAddress(command.email())).isPresent()) {
+        throw new IllegalArgumentException("Relative with email " + command.email() + " already exists.");
     }
+    try {
+        var relative = new Relative(command.email(),command.firstname(),command.lastname());
+        relativeRepository.save(relative);
+        return relative.getId();
+    }catch (Exception e){
+        throw new RuntimeException("Failed to create relative: " + e.getMessage(), e);
+    }
+    }
+    @Override
+    public Long handle(AssignUserToRelativeCommand command) {
+       var relative = relativeRepository.findByEmailAddress(new EmailAddress(command.email())).orElseThrow(() -> new IllegalArgumentException("Relative with email " + command.email() + " not found."));
+           relative.linkToUser(command.userId());
+           relativeRepository.save(relative);
+           return relative.getId();
+    }
+
 }
