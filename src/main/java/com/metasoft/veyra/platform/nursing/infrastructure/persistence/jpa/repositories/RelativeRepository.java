d8d55ec8ba04b3c1c72cfd03cf9ff514ed3eea3c
RenatoCY <renato.calvo2@hotmail.com>
2026-05-30 16:54:28 -0500

    feat(nursing): add methods to RelativeRepository and ResidentRepository for nursing home associations

--- src/main/java/com/metasoft/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/RelativeRepository.java
+++ src/main/java/com/metasoft/veyra/platform/nursing/infrastructure/persistence/jpa/repositories/RelativeRepository.java
@@ -5,12 +5,13 @@ import com.metasoft.veyra.platform.shared.domain.model.valueobjects.EmailAddress
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RelativeRepository extends JpaRepository<Relative,Long> {
+public interface RelativeRepository extends JpaRepository<Relative, Long> {
     boolean existsByUserId(UserId userId);
-
-
+   List<Relative>findAllByNursingHomeId(Long nursingHomeId);
+    boolean existsByEmailAddress(EmailAddress emailAddress);
     Optional<Relative> findByEmailAddress(EmailAddress emailAddress);
 }
