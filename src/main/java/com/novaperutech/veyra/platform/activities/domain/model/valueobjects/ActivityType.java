54ed75a7ef5c27b1c12dbdabbbe8818b3c96110a
Racso24k <u20211g192@upc.edu.pe>
2026-06-03 22:19:19 -0500

    feat(activities): add RECREATIONAL to ActivityType enum

--- src/main/java/com/novaperutech/veyra/platform/activities/domain/model/valueobjects/ActivityType.java
+++ src/main/java/com/novaperutech/veyra/platform/activities/domain/model/valueobjects/ActivityType.java
@@ -3,5 +3,6 @@ package com.novaperutech.veyra.platform.activities.domain.model.valueobjects;
 public enum ActivityType {
     MEAL,
     BATH,
-    RISK_PROFILE
+    RISK_PROFILE,
+    RECREATIONAL
 }
