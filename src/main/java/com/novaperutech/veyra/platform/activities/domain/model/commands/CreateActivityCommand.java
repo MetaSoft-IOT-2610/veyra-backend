9319dae445c3dd3f43c2b46472ec0e5003b33e8d
Racso24k <u20211g192@upc.edu.pe>
2026-06-03 22:19:25 -0500

    feat(activities): extend CreateActivityCommand with title, isRecurring and recurringDays

--- src/main/java/com/novaperutech/veyra/platform/activities/domain/model/commands/CreateActivityCommand.java
+++ src/main/java/com/novaperutech/veyra/platform/activities/domain/model/commands/CreateActivityCommand.java
@@ -1,9 +1,15 @@
 package com.novaperutech.veyra.platform.activities.domain.model.commands;
 
 import com.novaperutech.veyra.platform.activities.domain.model.valueobjects.ActivityType;
+import com.novaperutech.veyra.platform.activities.domain.model.valueobjects.RecurringDay;
+
+import java.util.List;
 
 public record CreateActivityCommand(
         Long residentId,
         Long healthcareStaffId,
-        ActivityType type
+        ActivityType type,
+        String title,
+        Boolean isRecurring,
+        List<RecurringDay> recurringDays
 ) {}
