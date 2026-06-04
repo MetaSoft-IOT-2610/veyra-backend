f98331874d65548678bb61f95df4c091815a6704
Racso24k <u20211g192@upc.edu.pe>
2026-06-03 22:28:55 -0500

    feat(shared): add IllegalStateException handler returning 409 CONFLICT

--- src/main/java/com/novaperutech/veyra/platform/shared/interfaces/rest/GlobalExceptionHandler.java
+++ src/main/java/com/novaperutech/veyra/platform/shared/interfaces/rest/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 /**
  * Global exception handler for REST controllers.
@@ -48,6 +49,12 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     ErrorResponse handleException(IllegalArgumentException ex) {
         return ErrorResponse.create(ex, HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value()), ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    ErrorResponse handleException(IllegalStateException ex) {
+        return ErrorResponse.create(ex, HttpStatusCode.valueOf(HttpStatus.CONFLICT.value()), ex.getMessage());
     }
 
 }
