049e21af88f658bc676815fb45bb9e0b118d55b3
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-06-08 21:49:58 -0500

    fix(nursing): add method to fetch administrator by user ID in NursingContextFacade

--- src/main/java/com/metasoft/veyra/platform/nursing/application/acl/NursingContextFacadeImpl.java
+++ src/main/java/com/metasoft/veyra/platform/nursing/application/acl/NursingContextFacadeImpl.java
@@ -1,9 +1,11 @@
 package com.metasoft.veyra.platform.nursing.application.acl;
 
+import com.metasoft.veyra.platform.nursing.domain.model.queries.GetAdministratorByUserIdQuery;
 import com.metasoft.veyra.platform.nursing.domain.model.queries.GetNursingHomeByIdQuery;
 import com.metasoft.veyra.platform.nursing.domain.model.queries.GetResidentByIdQuery;
 import com.metasoft.veyra.platform.nursing.domain.model.queries.GetResidentByPersonProfileQuery;
 import com.metasoft.veyra.platform.nursing.domain.model.valueobjects.PersonProfileId;
+import com.metasoft.veyra.platform.nursing.domain.services.AdministratorQueryService;
 import com.metasoft.veyra.platform.nursing.domain.services.NursingHomeQueryServices;
 import com.metasoft.veyra.platform.nursing.domain.services.ResidentQueryServices;
 import com.metasoft.veyra.platform.nursing.interfaces.acl.NursingContextFacade;
@@ -12,32 +14,41 @@ import org.springframework.stereotype.Service;
 @Service
 
 public class NursingContextFacadeImpl implements NursingContextFacade {
-    private final NursingHomeQueryServices nursingHomeQueryServices;
-   private final ResidentQueryServices residentQueryServices;
-    public NursingContextFacadeImpl(NursingHomeQueryServices nursingHomeQueryServices, ResidentQueryServices residentQueryServices) {
-        this.nursingHomeQueryServices = nursingHomeQueryServices;
-        this.residentQueryServices = residentQueryServices;
-    }
+  private final NursingHomeQueryServices nursingHomeQueryServices;
+  private final ResidentQueryServices residentQueryServices;
+  private final AdministratorQueryService administratorQueryService;
+  public NursingContextFacadeImpl(NursingHomeQueryServices nursingHomeQueryServices, ResidentQueryServices residentQueryServices, AdministratorQueryService administratorQueryService) {
+    this.nursingHomeQueryServices = nursingHomeQueryServices;
+    this.residentQueryServices = residentQueryServices;
+    this.administratorQueryService = administratorQueryService;
+  }
 
-    @Override
-    public Long fetchNursingHomeById(Long id ) {
-        var findNursingHomeById=new GetNursingHomeByIdQuery(id);
-        var  nursingHome=nursingHomeQueryServices.handle(findNursingHomeById);
-        return nursingHome.isEmpty()?Long.valueOf(0L):nursingHome.get().getId();
-    }
+  @Override
+  public Long fetchNursingHomeById(Long id ) {
+    var findNursingHomeById=new GetNursingHomeByIdQuery(id);
+    var  nursingHome=nursingHomeQueryServices.handle(findNursingHomeById);
+    return nursingHome.isEmpty()?Long.valueOf(0L):nursingHome.get().getId();
+  }
 
-    @Override
-    public boolean existsResidentByPersonProfile(Long id) {
-        var personProfile= new PersonProfileId(id);
-        var query= new GetResidentByPersonProfileQuery(personProfile);
-        var residentOpt= residentQueryServices.handle(query);
-        return residentOpt.isPresent();
-    }
+  @Override
+  public boolean existsResidentByPersonProfile(Long id) {
+    var personProfile= new PersonProfileId(id);
+    var query= new GetResidentByPersonProfileQuery(personProfile);
+    var residentOpt= residentQueryServices.handle(query);
+    return residentOpt.isPresent();
+  }
 
-    @Override
-    public Long fetchResidentById(Long residentId) {
-        var findResidentById= new GetResidentByIdQuery(residentId);
-        var resident= residentQueryServices.handle(findResidentById);
-        return resident.isEmpty()?Long.valueOf(0L):resident.get().getId();
-    }
+  @Override
+  public Long fetchResidentById(Long residentId) {
+    var findResidentById= new GetResidentByIdQuery(residentId);
+    var resident= residentQueryServices.handle(findResidentById);
+    return resident.isEmpty()?Long.valueOf(0L):resident.get().getId();
+  }
+  @Override
+  public Long fetchAdministratorByUserId(Long userId) {
+    var findAdministratorByUserId= new GetAdministratorByUserIdQuery(userId);
+    var administrator= administratorQueryService.handle(findAdministratorByUserId);
+    return administrator.isEmpty()?Long.valueOf(0L):administrator.get().getId();
+
+  }
 }
--- src/main/java/com/metasoft/veyra/platform/nursing/interfaces/acl/NursingContextFacade.java
+++ src/main/java/com/metasoft/veyra/platform/nursing/interfaces/acl/NursingContextFacade.java
@@ -1,7 +1,8 @@
 package com.metasoft.veyra.platform.nursing.interfaces.acl;
 
 public interface NursingContextFacade {
-    Long fetchNursingHomeById(Long id);
-    boolean existsResidentByPersonProfile(Long personProfile);
-    Long fetchResidentById(Long residentId);
+  Long fetchNursingHomeById(Long id);
+  boolean existsResidentByPersonProfile(Long personProfile);
+  Long fetchResidentById(Long residentId);
+  Long fetchAdministratorByUserId(Long userId);
 }
