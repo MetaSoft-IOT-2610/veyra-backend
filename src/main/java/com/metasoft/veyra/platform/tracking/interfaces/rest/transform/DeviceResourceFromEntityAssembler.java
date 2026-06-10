2d3fabec449a652fd58ad8615ec07111faa1dd06
RenatoCY <renato.calvo2@hotmail.com>
2026-06-10 11:58:06 -0500

    refactor(tracking): improve resource assemblers

--- src/main/java/com/metasoft/veyra/platform/tracking/interfaces/rest/transform/DeviceResourceFromEntityAssembler.java
+++ src/main/java/com/metasoft/veyra/platform/tracking/interfaces/rest/transform/DeviceResourceFromEntityAssembler.java
@@ -4,18 +4,16 @@ import com.metasoft.veyra.platform.tracking.domain.model.aggregates.Device;
 import com.metasoft.veyra.platform.tracking.interfaces.rest.resources.DeviceResource;
 
 public class DeviceResourceFromEntityAssembler {
-    public static DeviceResource toResourceFromEntity(Device device) {
-        String lastSync = device.getAssignedAt() != null ? device.getAssignedAt().toString() : null;
+    public static DeviceResource toResourceFromEntity(Device entity) {
+        var residentId= entity.getResidentId()!= null ? entity.getResidentId().residentId():null;
         return new DeviceResource(
-                device.getId(),
-                device.getDeviceId(),
-                device.getNursingHomeId(),
-                device.getDeviceType() != null ? device.getDeviceType().name() : null,
-                device.getResidentId(),
-                device.getAssignedBy(),
-                lastSync,
-                lastSync,
-                device.getStatus().name()
+               entity.getId(),
+               entity.getNursingHomeId().nursingHomeId(),
+               entity.getDeviceType().name(),
+               entity.getStatus().name(),
+               residentId,
+               entity.getAssignedAt(),
+               entity.getMacAddress().macAddress()
         );
     }
 }
