5d15c523913da981806917b736f43a914f8f3865
RenatoCY <renato.calvo2@hotmail.com>
2026-06-10 12:06:58 -0500

    feat(tracking): add residentId and nursingHomeId and revise device statuses

--- src/main/java/com/metasoft/veyra/platform/tracking/domain/model/valueobjects/DeviceId.java
+++ src/main/java/com/metasoft/veyra/platform/tracking/domain/model/valueobjects/DeviceId.java
@@ -1,10 +1,10 @@
 package com.metasoft.veyra.platform.tracking.domain.model.valueobjects;
 
 
-public record DeviceId(String deviceId) {
+public record DeviceId(Long deviceId) {
 
     public DeviceId {
-        if (deviceId == null || deviceId.isBlank()) {
+        if (deviceId == null) {
             throw new IllegalArgumentException("Device ID cannot be null or empty");
         }
     }
