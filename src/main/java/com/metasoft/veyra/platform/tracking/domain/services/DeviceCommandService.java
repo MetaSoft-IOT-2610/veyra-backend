1d43f4b36ba3d0b7c474b568b5cce077470c3dd1
RenatoCY <renato.calvo2@hotmail.com>
2026-06-11 16:48:00 -0500

    refactor(tracking): remove unused DeleteDeviceCommand from DeviceCommandService

--- src/main/java/com/metasoft/veyra/platform/tracking/domain/services/DeviceCommandService.java
+++ src/main/java/com/metasoft/veyra/platform/tracking/domain/services/DeviceCommandService.java
@@ -2,7 +2,6 @@ package com.metasoft.veyra.platform.tracking.domain.services;
 
 import com.metasoft.veyra.platform.tracking.domain.model.commands.AssignDeviceCommand;
 import com.metasoft.veyra.platform.tracking.domain.model.commands.ChangeDeviceStatusCommand;
-import com.metasoft.veyra.platform.tracking.domain.model.commands.DeleteDeviceCommand;
 import com.metasoft.veyra.platform.tracking.domain.model.commands.RegisterDeviceCommand;
 import com.metasoft.veyra.platform.tracking.domain.model.commands.UnassignDeviceCommand;
 import com.metasoft.veyra.platform.tracking.domain.model.commands.UpdateDeviceCommand;
@@ -12,6 +11,5 @@ public interface DeviceCommandService {
     void handle(UnassignDeviceCommand command);
     Long handle(RegisterDeviceCommand command);
     Long handle(UpdateDeviceCommand command);
-    void handle(DeleteDeviceCommand command);
     Long handle(ChangeDeviceStatusCommand command);
 }
