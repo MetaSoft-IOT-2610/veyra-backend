25de822bed1c2178b5e21a2294a027569ff9ed99
RenatoCY <renato.calvo2@hotmail.com>
2026-06-12 19:27:29 -0500

    refactor(analytics): update year validation logic to use current year

--- src/main/java/com/metasoft/veyra/platform/analytics/domain/model/queries/GetResidentActivesByNursingHomeIdAndYearQuery.java
+++ src/main/java/com/metasoft/veyra/platform/analytics/domain/model/queries/GetResidentActivesByNursingHomeIdAndYearQuery.java
@@ -2,15 +2,16 @@ package com.metasoft.veyra.platform.analytics.domain.model.queries;
 
 import com.metasoft.veyra.platform.analytics.domain.model.valueobjects.NursingHomeId;
 
+import java.time.Year;
+
 public record GetResidentActivesByNursingHomeIdAndYearQuery(NursingHomeId nursingHomeId , Integer year) {
-    public GetResidentActivesByNursingHomeIdAndYearQuery{
-        if (year==null){
+    public GetResidentActivesByNursingHomeIdAndYearQuery {
+        if (year == null) {
             throw new IllegalArgumentException("year cannot be null");
         }
-        if ( year < 1900 || year > 2025) {
-            throw new IllegalArgumentException("year must be between 1900 and 2025");
+        if (year < 1900 || year > Year.now().getValue()) {
+            throw new IllegalArgumentException("year must be between 1900 and " + Year.now().getValue());
         }
-
     }
 
 }
