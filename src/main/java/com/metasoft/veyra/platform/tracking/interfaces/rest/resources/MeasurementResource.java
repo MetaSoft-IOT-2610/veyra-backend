be36662ec723a6eba9093dd3358597e2065532df
RenatoCY <renato.calvo2@hotmail.com>
2026-06-21 09:40:32 -0500

    feat(measurements): extend MeasurementResource with additional measurement fields and add RecordMeasurementResource class

--- src/main/java/com/metasoft/veyra/platform/tracking/interfaces/rest/resources/MeasurementResource.java
+++ src/main/java/com/metasoft/veyra/platform/tracking/interfaces/rest/resources/MeasurementResource.java
@@ -1,4 +1,7 @@
 package com.metasoft.veyra.platform.tracking.interfaces.rest.resources;
 
-public record MeasurementResource(String  id ,Long deviceId) {
+import java.time.LocalDateTime;
+
+public record MeasurementResource(String  id , Long deviceId, Double temperature, Integer heartRate, Integer oxygenSaturation,
+                                  LocalDateTime timestamp) {
 }
